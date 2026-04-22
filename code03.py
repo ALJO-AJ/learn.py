e3c8773cb272bdbc806184d222875e56de71ea4d
Aljoaj <85579877+ALJO-AJ@users.noreply.github.com>
2026-04-22 21:44:00 +0530

    Create code03.py for adding two numbers
    Add a Python script to sum two numbers with user input.

---
+++ code03.py
@@ -0,0 +1,11 @@
+#write a python code to add two number then take input add 2 numbers
+num1 = 10
+num2 = 45
+total = num1 + num2
+print(f'the sum is {total}' );
+
+num3 =int(input("enter first number- "))
+num4 =int(input("enter second number- "))
+
+result = (num3 + num4)
+print(f"the answer is {result}");
