d848a206c8e00ad02a6cadfb4191a10d282611ec
Aljoaj <85579877+ALJO-AJ@users.noreply.github.com>
2026-04-22 21:59:21 +0530

    Add odd or even checker in code04.py

---
+++ code04.py
@@ -0,0 +1,7 @@
+#write a code to understand if the input is odd or even
+num = int(input("Enter a number to check if odd or even- "))
+if num % 2==0:
+    print(f"the {num} is even");
+
+else:
+    print("the number is odd");
