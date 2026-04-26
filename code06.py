7e277318e6d99c8c3d0319825493901766716ab3
Aljoaj <85579877+ALJO-AJ@users.noreply.github.com>
2026-04-26 12:07:54 +0530

    Add code to calculate and print remainder

---
+++ code06.py
@@ -0,0 +1,7 @@
+#Write a code to find reminder
+
+num1 =int(input("print first number  - "))
+num2 = int(input("print second number - "))
+reminder = num1 % num2
+
+print(f'the reminder is -{reminder}')
